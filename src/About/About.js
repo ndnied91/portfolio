f475213233370e3de8ca17452cb95102ed3e8b5b
danny <danielniedzwiedzki.1@gmail.com>
2021-08-22 18:15:37 -0400

    update

--- src/About/About.js
+++ src/About/About.js
@@ -24,7 +24,8 @@ class Info extends React.Component{
                   <p className="about_content">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu orci in enim rutrum ultrices sit amet vel dolor.
                     Pellentesque dictum lacinia enim sit amet lobortis. Sed et nulla quis eros gravida pretium.
-                    Integer interdum semper urna, sed mollis leo fermentum quis. Nulla in mauris vitae massa volutpat tincidunt. Pellentesque quis ante egestas nulla egestas eleifend sed nec mauris. Maecenas in erat id risus placerat posuere. Pellentesque non quam elit. Pellentesque suscipit turpis eget lacus feugiat, vel auctor nibh pulvinar. Nulla vitae velit et mi lobortis commodo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis vitae gravida mi, non auctor metus. Donec et lacinia est. Quisque vel mauris molestie, malesuada lorem sit amet, molestie risus. Fusce condimentum purus eget mattis consequat. Etiam pharetra finibus turpis.
+                    Integer interdum semper urna, sed mollis leo fermentum quis. Nulla in mauris vitae massa volutpat tincidunt.
+                    Pellentesque quis ante egestas nulla egestas eleifend sed nec mauris. Maecenas in erat id risus placerat posuere.
 
                     What I enjoy
                   </p>
@@ -60,7 +61,7 @@ class Info extends React.Component{
 
         <Fader>
           <div className="">
-            <section className="extra-stuff move_up">
+            <section className="extra-stuff">
                <h4 className="about_title"> Extra Stuff</h4>
 
                   Courses: Udemy Courses

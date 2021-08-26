de4f65f1742b834dab8049a018d5f6bd2e0a753e
danny <danielniedzwiedzki.1@gmail.com>
2021-08-26 11:33:13 -0400

    update

--- src/Resume/Resume.js
+++ src/Resume/Resume.js
@@ -2,11 +2,7 @@ import React from 'react'
 import './resume.css'
 
 const Resume = () =>(
-  <div style={{'height': '1000px'}}>
-    <object data="/assets/Daniel_Niedzwiedzki_Resume.pdf" height="100%" width="100%" type="application/pdf">
-         <iframe src="/assets/Daniel_Niedzwiedzki_Resume.pdf"  height="1000px" width="100%" type="application/pdf" />
-    </object>
-  </div>
+    <iframe className="resume" title='uniqueTitle' src="/assets/Daniel_Niedzwiedzki_Resume.pdf" />
 )
 
 
@@ -14,3 +10,11 @@ export default Resume
 
 
 // <iframe className="resume" title='uniqueTitle' src="/assets/Daniel_Niedzwiedzki_Resume.pdf" />
+
+
+
+// <div style={{'height': '1000px'}}>
+//   <object data="/assets/Daniel_Niedzwiedzki_Resume.pdf" height="100%" width="100%" type="application/pdf">
+//        <iframe src="/assets/Daniel_Niedzwiedzki_Resume.pdf"  height="1000px" width="100%" type="application/pdf" />
+//   </object>
+// </div>

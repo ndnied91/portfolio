3288458ea4147eba70381a7bcbe451c33025295f
danny <danielniedzwiedzki.1@gmail.com>
2021-09-10 12:02:14 -0400

    added project

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -9,7 +9,7 @@ class Projects extends React.Component{
   constructor(){
     super()
         this.state={ showPortfolio: false , showPokemon: false , showCovid: false, showTravelBlog: false , showimageClass: false
-        ,showDormRental: false}
+        ,showDormRental: false , twitterTracker: false}
   }
   render(){
 
@@ -39,6 +39,11 @@ switch(project){
       this.state.showDormRental ? this.setState({showDormRental: false}) :  this.setState({showDormRental: true})
       break;
 
+      case 'twitterTracker':
+       this.state.twitterTracker ? this.setState({twitterTracker: false}) :  this.setState({twitterTracker: true})
+       break;
+
+
          default:
             return null
         }
@@ -64,7 +69,7 @@ switch(project){
                       <div className="desc-learn effect1" onClick={()=>changeState('portfolio')}>
                           {this.state.showPortfolio ?
                               <UniModal link={'/'}
-                                  desc={' A portfolio project used to present all my work / background and info. Build with ReactJS, styled with SASS/CSS and hosted on Heroku'} title={'Portfolio'}
+                                  desc={'A portfolio project used to present all my work / background and info. Build with ReactJS, styled with SASS/CSS and hosted on Heroku'} title={'Portfolio'}
                                   lang={['JavaScript' , 'ReactJS' , 'HTML' ,'CSS' , 'SCSS'] }
                                   github={'/https://github.com/ndnied91/portfolio'}
                                           /> : <div className="learn_btn"> LEARN MORE </div>  }
@@ -82,7 +87,7 @@ switch(project){
                     <div className="desc-learn effect1" onClick={()=>changeState('pokemon')}>
                         {this.state.showPokemon ?
                           <UniModal link={'https://github.com/ndnied91/whos_that_pokemon'}
-                              desc={'The classic "Whos That Pokemon!?" game from the Pokemon TV show. This project was created with ReactJS as the Front End library. An async call is made to a Pokemon API which retrieves all the necessary data for that specific Pokemon and renders the info for the user to guess'} title={'Pokemon Guessing Game'}
+                              desc={"The classic 'Who's That Pokemon!?' game from the Pokemon TV show. This project was created with ReactJS as the Front-End User Interface. An async call is made to a Pokemon API which retrieves all the necessary data for a specific Pokemon and renders the info for the user to guess."} title={'Pokemon Guessing Game'}
                               lang={['JavaScript' , 'ReactJS' , 'HTML' ,'CSS' , 'SASS'] }
                               github={'https://github.com/ndnied91/whos_that_pokemon'}
                                       />
@@ -100,7 +105,7 @@ switch(project){
                       <div className="desc-learn effect1" onClick={()=>changeState('covid')}>
                           {this.state.showCovid ?
                             <UniModal link={'http://covidvisualizer.herokuapp.com'}
-                                desc={'A visualizer depicting Covid-19 cases in relation to income/population on a state/county level. NodeJS used for all server-side rendering which includes a custom API and a scheduler that retrieves all Covid-19 data autonomously.  '} title={'Covid-19 Visualizer'}
+                                desc={'A visualizer depicting Covid-19 cases in relation to income/population on a state/county level. NodeJS used for all server-side rendering which includes a custom API and a scheduler that retrieves all Covid-19 data autonomously.'} title={'Covid-19 Visualizer'}
                                 lang={['JavaScript' , 'ReactJS' , 'HTML' ,'CSS' , 'SASS' , 'Redux' , 'NodeJS' , 'MongoDB'] }
                                 github={'https://github.com/ndnied91/covidTracker'}
                                         />
@@ -119,7 +124,7 @@ switch(project){
                          {this.state.showTravelBlog ?
 
                            <UniModal link={'https://traveling-the-world.herokuapp.com'}
-                               desc={'A Travel blog developed with ReactJS and NodeJS. All travel blogs are saved within a MongoDB database. Redux used for all state management. PassportJS used for all user authentication.'} title={'Travel Blog'}
+                               desc={'A travel blog developed with ReactJS and NodeJS. All travel blogs are saved within a MongoDB database. Redux used for all state management. PassportJS used for all user authentication.'} title={'Travel Blog'}
                                lang={['JavaScript' , 'ReactJS' , 'HTML' ,'CSS' , 'SASS' , 'Redux' , 'NodeJS' , 'MongoDB'] }
                                github={'https://github.com/ndnied91/blog_project'}
                                        />
@@ -138,7 +143,7 @@ switch(project){
                         {this.state.showimageClass ?
                           <UniModal link={'http://yoda.kean.edu/~niedzwid/CPS4721/hw3.1.html'}
 
-                              desc={'Image Classifier which utilizies TensorFlow for machine learning. MySQL used for data storage. Unix shell script used as a mediatory between user interface and machine learning algorithm.'}
+                              desc={'Image Classifier which utilizes TensorFlow for machine learning. MySQL used for data storage. Unix shell script used as a mediatory between user interface and machine learning algorithm.'}
 
                               title={'Image Classifer'}
                               lang={['JavaScript' ,'Python' , 'PHP' , 'Bash' , 'CSS' , 'HTML' ] }
@@ -157,7 +162,7 @@ switch(project){
                        {this.state.showDormRental ?
                          <UniModal
                             link={'https://enigmatic-bastion-91344.herokuapp.com'}
-                             desc={'Dorm rental system which enables students to rent out individual dorms for the semester. HTML/CSS used for all front end and NodeJS used for backend API routing. MongoDB used to store all user and dorm data.'} title={'Kean Dorm Rental'}
+                             desc={'Dorm rental system which enables students to rent out individual dorms for the semester. HTML/CSS used for all front-end and NodeJS used for backend API routing. MongoDB used to store all user and dorm data. An admin dashboard is developed to show currently rented out and available dorms.'} title={'Kean Dorm Rental'}
                              lang={['JavaScript' , 'ReactJS' , 'HTML' ,'CSS' , 'SCSS' , 'Redux' , 'NodeJS' , 'MongoDB'] }
                              github={'https://github.com/ndnied91/Dorm-Project'}
                                      />
@@ -166,9 +171,25 @@ switch(project){
                    <img className="pic kean effect" src="assets/kean1.png" alt='kean' />
             </div>
 
+            <div className='project-desc'>
+                  <a className="desc-title effect1" href='http://eve.kean.edu/~niedzwid/CPS4721/indexProject.html'  target="_blank" rel="noreferrer">NBA Performace Tracker</a>
+                  <div className="desc effect1">  <span>  Python | MySQL | JavaScript </span>  </div>
+                  <div className="desc-learn effect1" onClick={()=>changeState('twitterTracker')}>
+                      {this.state.twitterTracker ?
+                        <UniModal
+                           link={'http://eve.kean.edu/~niedzwid/CPS4721/indexProject.html'}
+                            desc={"Research project which utilizes JavaScript to collect Twitter info from a given player, this data is sorted and saved into a MySQL database. PHP is used for data analysis and visualization. A correlation graph is developed to display a player's popularity in regards to their in-game performance."} title={'NBA Performace Tracker'}
+                            lang={['PHP' , 'JavaScript' , 'HTML' ,'CSS' , 'NodeJS' , 'MySQL'] }
+                            github={'https://github.com/ndnied91/Dorm-Project'}
+                                    />
+                         : <div className="learn_btn"> LEARN MORE </div>  }
+                  </div>
+                  <img className="pic kean effect" src="assets/nba.png" alt='kean' />
+           </div>
 
 
-            </div>
+
+          </div> {/* project grid */}
 
           </section>
 
